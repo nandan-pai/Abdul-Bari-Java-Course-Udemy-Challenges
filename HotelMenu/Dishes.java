7bc80bfd6b3f8de8602e299922c1e3139f8909fe
nandan-pai <53964874+nandan-pai@users.noreply.github.com>
2021-07-22 18:01:10 +0530

    Dish Price Search Added

--- HotelMenu/Dishes.java
+++ HotelMenu/Dishes.java
@@ -11,7 +11,7 @@ class Dishes {
 class Menu {
 	Dishes dishesMenu[] = new Dishes[100];
 	int dishCount = 0;
-	
+
 	public void addItem(String dName, int price) {
 		dishesMenu[dishCount] = new Dishes(dName, price);
 		dishCount++;
@@ -24,5 +24,14 @@ class Menu {
 			System.out.format("%s%-20s %d\n","\t",dishesMenu[i].dishName, dishesMenu[i].price);
 		}
 		System.out.println();
+	}
+	
+	public String getPrice(String dish) {
+		for(int i = 0 ; i < dishCount ; i++) {
+			if(dish.equals(dishesMenu[i].dishName)) {
+				return "The price of " + dish + " is = " + dishesMenu[i].price;
+			}
+		}
+		return dish + " is not there in the menu";
 	}
 }
